720dd35425859c02758e86154d243d24ae79433d
drobiss <brezinadavid93@icloud.com>
2024-07-14 13:30:11 +0200

    weight arrows added + overscroll fixed

--- src/components/WeightInfo.jsx
+++ src/components/WeightInfo.jsx
@@ -1,8 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
+
 const WeightInfo = ({ weight }) => {
   return (
     <div className="weight-info">
-      <p>Aktuální váha: {weight?.current || "Nezadáno"}</p>
-      <p>Předchozí váha: {weight?.previous || "Nezadáno"}</p>
+      <p><FontAwesomeIcon icon={faArrowRight} className='currentW-icon'/> Aktuální váha: {weight?.current || "Nezadáno"}</p>
+      <p><FontAwesomeIcon icon={faArrowRight} className='lastW-icon'/> Předchozí váha: {weight?.previous || "Nezadáno"}</p>
     </div>
   )
 }
